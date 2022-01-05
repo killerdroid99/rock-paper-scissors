cb2315734e82b4911112b574fa279dc19b9fd4b1
Aniket Jena <75910246+killerdroid99@users.noreply.github.com>
2022-01-05 20:49:40 +0530

    Added styles and functionality in styles.css and app.js

--- app.js
+++ app.js
@@ -1,32 +1,116 @@
+const rock = document
+	.getElementById("rock")
+	.addEventListener("click", function () {
+		playRound("rock");
+	});
+
+const paper = document
+	.getElementById("paper")
+	.addEventListener("click", function () {
+		playRound("paper");
+	});
+
+const scissors = document
+	.getElementById("scissors")
+	.addEventListener("click", function () {
+		playRound("scissors");
+	});
+
+let playerScore = document.getElementById("player-score-value");
+let compScore = document.getElementById("comp-score-value");
 let rpsArray = ["rock", "paper", "scissors"];
+let statement = document.getElementById("result_statement");
+let btn = document.getElementById("button");
+
+playerScore.innerHTML = 0;
+compScore.innerHTML = 0;
 
 function compPlay() {
 	return rpsArray[Math.floor(Math.random() * 3)];
 }
 
-function playRound(player, comp) {
-	if (player == "rock" && comp == "rock") {
-		console.log(`player - ${player} : computer - ${comp} Tie!`);
-	} else if (player == "rock" && comp == "paper") {
-		console.log(`player - ${player} : computer - ${comp} comp wins!`);
-	} else if (player == "rock" && comp == "scissors") {
-		console.log(`player - ${player} : computer - ${comp} player wins!`);
-	} else if (player == "paper" && comp == "paper") {
-		console.log(`player - ${player} : computer - ${comp} Tie!`);
-	} else if (player == "paper" && comp == "scissors") {
-		console.log(`player - ${player} : computer - ${comp} comp wins!`);
-	} else if (player == "paper" && comp == "rock") {
-		console.log(`player - ${player} : computer - ${comp} player wins!`);
-	} else if (player == "scissors" && comp == "scissors") {
-		console.log(`player - ${player} : computer - ${comp} Tie!`);
-	} else if (player == "scissors" && comp == "rock") {
-		console.log(`player - ${player} : computer - ${comp} comp wins!`);
-	} else if (player == "scissors" && comp == "paper") {
-		console.log(`player - ${player} : computer - ${comp} player wins!`);
-	} else {
-		console.log("Enter rock, paper or scissors");
+function playRound(player) {
+	let comp = compPlay();
+
+	if (
+		player == "rock" &&
+		comp == "rock" &&
+		playerScore.innerHTML < 5 &&
+		compScore.innerHTML < 5
+	) {
+		statement.innerHTML = `Player => ${player} : Computer => ${comp} <br />   Tie!`;
+	} else if (
+		player == "rock" &&
+		comp == "paper" &&
+		playerScore.innerHTML < 5 &&
+		compScore.innerHTML < 5
+	) {
+		statement.innerHTML = `Player => ${player} : Computer => ${comp} <br /> comp wins!`;
+		compScore.innerHTML++;
+	} else if (
+		player == "rock" &&
+		comp == "scissors" &&
+		playerScore.innerHTML < 5 &&
+		compScore.innerHTML < 5
+	) {
+		statement.innerHTML = `Player => ${player} : Computer => ${comp} <br /> player wins!`;
+		playerScore.innerHTML++;
+	} else if (
+		player == "paper" &&
+		comp == "paper" &&
+		playerScore.innerHTML < 5 &&
+		compScore.innerHTML < 5
+	) {
+		statement.innerHTML = `Player => ${player} : Computer => ${comp} <br /> Tie!`;
+	} else if (
+		player == "paper" &&
+		comp == "scissors" &&
+		playerScore.innerHTML < 5 &&
+		compScore.innerHTML < 5
+	) {
+		statement.innerHTML = `Player => ${player} : Computer => ${comp} <br /> comp wins!`;
+		compScore.innerHTML++;
+	} else if (
+		player == "paper" &&
+		comp == "rock" &&
+		playerScore.innerHTML < 5 &&
+		compScore.innerHTML < 5
+	) {
+		statement.innerHTML = `Player => ${player} : Computer => ${comp} <br /> player wins!`;
+		playerScore.innerHTML++;
+	} else if (
+		player == "scissors" &&
+		comp == "scissors" &&
+		playerScore.innerHTML < 5 &&
+		compScore.innerHTML < 5
+	) {
+		statement.innerHTML = `Player => ${player} : Computer => ${comp} <br /> Tie!`;
+	} else if (
+		player == "scissors" &&
+		comp == "rock" &&
+		playerScore.innerHTML < 5 &&
+		compScore.innerHTML < 5
+	) {
+		statement.innerHTML = `Player => ${player} : Computer => ${comp} <br /> comp wins!`;
+		compScore.innerHTML++;
+	} else if (
+		player == "scissors" &&
+		comp == "paper" &&
+		playerScore.innerHTML < 5 &&
+		compScore.innerHTML < 5
+	) {
+		statement.innerHTML = `Player => ${player} : Computer => ${comp} <br /> player wins!`;
+		playerScore.innerHTML++;
+	}
+	if (playerScore.innerHTML == 5) {
+		statement.innerHTML = "YOU WON THE GAME!!!";
+		statement.style.color = "rgb(0, 255, 13)";
+		btn.style.visibility = "visible";
+		btn.style.color = "rgb(0, 255, 13)";
+	}
+	if (compScore.innerHTML == 5) {
+		statement.innerHTML = "OH NO YOU LOST!";
+		statement.style.color = "rgb(255, 11, 11)";
+		btn.style.visibility = "visible";
 	}
 }
-const playerSelection = window.prompt("Rock, Paper or Scissors?");
-
-playRound(playerSelection.toLowerCase(), compPlay());
