2d76afec9baf9d7dc0db7dc0b6cb798ffc3b7e88
Aniket Jena <75910246+killerdroid99@users.noreply.github.com>
2022-01-08 12:32:04 +0530

    Replaced .innerHTML with .innerText at many instances

--- app.js
+++ app.js
@@ -22,8 +22,8 @@ let rpsArray = ["rock", "paper", "scissors"];
 let statement = document.getElementById("result_statement");
 let btn = document.getElementById("button");
 
-playerScore.innerHTML = 0;
-compScore.innerHTML = 0;
+playerScore.innerText = 0;
+compScore.innerText = 0;
 
 function compPlay() {
 	return rpsArray[Math.floor(Math.random() * 3)];
@@ -35,81 +35,90 @@ function playRound(player) {
 	if (
 		player == "rock" &&
 		comp == "rock" &&
-		playerScore.innerHTML < 5 &&
-		compScore.innerHTML < 5
+		playerScore.innerText < 5 &&
+		compScore.innerText < 5
 	) {
-		statement.innerHTML = `Player => ${player} : Computer => ${comp} <br />   Tie!`;
+		statement.innerText =
+			`Player => ${player} : Computer => ${comp}` + "\n" + `Tie!`;
 	} else if (
 		player == "rock" &&
 		comp == "paper" &&
-		playerScore.innerHTML < 5 &&
-		compScore.innerHTML < 5
+		playerScore.innerText < 5 &&
+		compScore.innerText < 5
 	) {
-		statement.innerHTML = `Player => ${player} : Computer => ${comp} <br /> comp wins!`;
-		compScore.innerHTML++;
+		statement.innerText =
+			`Player => ${player} : Computer => ${comp}` + "\n" + `comp wins!`;
+		compScore.innerText++;
 	} else if (
 		player == "rock" &&
 		comp == "scissors" &&
-		playerScore.innerHTML < 5 &&
-		compScore.innerHTML < 5
+		playerScore.innerText < 5 &&
+		compScore.innerText < 5
 	) {
-		statement.innerHTML = `Player => ${player} : Computer => ${comp} <br /> player wins!`;
-		playerScore.innerHTML++;
+		statement.innerText =
+			`Player => ${player} : Computer => ${comp}` + "\n" + `player wins!`;
+		playerScore.innerText++;
 	} else if (
 		player == "paper" &&
 		comp == "paper" &&
-		playerScore.innerHTML < 5 &&
-		compScore.innerHTML < 5
+		playerScore.innerText < 5 &&
+		compScore.innerText < 5
 	) {
-		statement.innerHTML = `Player => ${player} : Computer => ${comp} <br /> Tie!`;
+		statement.innerText =
+			`Player => ${player} : Computer => ${comp}` + "\n" + ` Tie!`;
 	} else if (
 		player == "paper" &&
 		comp == "scissors" &&
-		playerScore.innerHTML < 5 &&
-		compScore.innerHTML < 5
+		playerScore.innerText < 5 &&
+		compScore.innerText < 5
 	) {
-		statement.innerHTML = `Player => ${player} : Computer => ${comp} <br /> comp wins!`;
-		compScore.innerHTML++;
+		statement.innerText =
+			`Player => ${player} : Computer => ${comp}` + "\n" + `comp wins!`;
+		compScore.innerText++;
 	} else if (
 		player == "paper" &&
 		comp == "rock" &&
-		playerScore.innerHTML < 5 &&
-		compScore.innerHTML < 5
+		playerScore.innerText < 5 &&
+		compScore.innerText < 5
 	) {
-		statement.innerHTML = `Player => ${player} : Computer => ${comp} <br /> player wins!`;
-		playerScore.innerHTML++;
+		statement.innerText =
+			`Player => ${player} : Computer => ${comp}` + "\n" + `player wins!`;
+		playerScore.innerText++;
 	} else if (
 		player == "scissors" &&
 		comp == "scissors" &&
-		playerScore.innerHTML < 5 &&
-		compScore.innerHTML < 5
+		playerScore.innerText < 5 &&
+		compScore.innerText < 5
 	) {
-		statement.innerHTML = `Player => ${player} : Computer => ${comp} <br /> Tie!`;
+		statement.innerText =
+			`Player => ${player} : Computer => ${comp}` + "\n" + `Tie!`;
 	} else if (
 		player == "scissors" &&
 		comp == "rock" &&
-		playerScore.innerHTML < 5 &&
-		compScore.innerHTML < 5
+		playerScore.innerText < 5 &&
+		compScore.innerText < 5
 	) {
-		statement.innerHTML = `Player => ${player} : Computer => ${comp} <br /> comp wins!`;
-		compScore.innerHTML++;
+		statement.innerText =
+			`Player => ${player} : Computer => ${comp}` + "\n" + `comp wins!`;
+		compScore.innerText++;
 	} else if (
 		player == "scissors" &&
 		comp == "paper" &&
-		playerScore.innerHTML < 5 &&
-		compScore.innerHTML < 5
+		playerScore.innerText < 5 &&
+		compScore.innerText < 5
 	) {
-		statement.innerHTML = `Player => ${player} : Computer => ${comp} <br /> player wins!`;
-		playerScore.innerHTML++;
+		statement.innerText =
+			`Player => ${player} : Computer => ${comp}` + "\n" + `player wins!`;
+		playerScore.innerText++;
 	}
-	if (playerScore.innerHTML == 5) {
-		statement.innerHTML = "YOU WON THE GAME!!!";
+	if (playerScore.innerText == 5) {
+		statement.innerText = "YOU WON THE GAME!!!";
 		statement.style.color = "rgb(0, 255, 13)";
 		btn.style.visibility = "visible";
 		btn.style.color = "rgb(0, 255, 13)";
 	}
-	if (compScore.innerHTML == 5) {
-		statement.innerHTML = "OH NO YOU LOST!";
+	if (compScore.innerText == 5) {
+		statement.innerText = "OH NO YOU LOST!";
 		statement.style.color = "rgb(255, 11, 11)";
 		btn.style.visibility = "visible";
 	}
